11330a06a095aa824905de6ef025e5058f5af046
Dorian-Cesar <dorian.celu@gmail.com>
2024-09-05 13:19:24 -0400

    add htacces2

--- backend/obtener_ciudades.php
+++ backend/obtener_ciudades.php
@@ -4,7 +4,16 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");  // Métodos permiti
 header("Access-Control-Allow-Headers: Content-Type, Authorization");  // Encabezados permitidos
 
 
-include 'conexion.php';
+$servername = "ls-8ce02ad0b7ea586d393e375c25caa3488acb80a5.cylsiewx0zgx.us-east-1.rds.amazonaws.com";
+$username = "dbmasteruser";
+$password = ':&T``E~r:r!$1c6d:m143lzzvGJ$NuP;';
+$dbname = "equipaje_bus";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 
 $sql = "SELECT nombre FROM ciudades order by nombre";
 $result = $conn->query($sql);
