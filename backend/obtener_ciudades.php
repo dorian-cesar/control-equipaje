b8ada899154f4166037fcb78b45063ee00225d73
Dorian-Cesar <dorian.celu@gmail.com>
2024-09-05 12:36:19 -0400

    fix cors

--- backend/obtener_ciudades.php
+++ backend/obtener_ciudades.php
@@ -1,4 +1,8 @@
 <?php
+
+header('Access-Control-Allow-Origin: *'); // Permitir todas las orígenes
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE'); // Permitir los métodos HTTP
+header('Access-Control-Allow-Headers: Content-Type, Authorization'); // Permitir las cabeceras
 include 'conexion.php';
 
 $sql = "SELECT nombre FROM ciudades order by nombre";
