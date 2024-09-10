bbfd08b56dbd0d3beda3adc83585fafce7456700
Dorian-Cesar <dorian.celu@gmail.com>
2024-09-10 12:32:52 -0300

    nuevo ticket 02

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
         //registro en bbdd
         //https://araucania.wit.la/control-equipaje/backend/registrar.php
 
-        $.post('https://araucania.wit.la/backend/registrar.php', formData, function (data) {
+        $.post('https://araucania.wit.la/control-equipaje/backend/registrar.php', formData, function (data) {
             $('#ticket').html(data);
             window.location = 'printerplus://send?text=' + document.getElementById('ticket').innerHTML;
             $('#imprimirEquipaje').show(); // Mostrar botón para empezar a imprimir maletas
@@ -94,7 +94,7 @@ $(document).ready(function () {
             formData += '&fechaHoraViaje=' + configuracionViaje.fechaHoraViaje;
             // Añadimos el número de la pieza actual
 
-            $.post('https://araucania.wit.la/backend/imprimir_equipaje.php', formData, function (data) {
+            $.post('https://araucania.wit.la/control-equipaje/backend/imprimir_equipaje.php', formData, function (data) {
                 $('#ticket').html(data);
                 $('#imprimirEquipaje').text('Imprimir: '+(equipajeActual+1) + ' de ' + totalEquipaje);
                 window.location = 'printerplus://send?text=' + document.getElementById('ticket').innerHTML;
